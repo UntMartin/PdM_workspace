9082c03a4441fe44c05179e5ed7650ecb86d6572
Martin Juarez <mjuarez@herrera.unt.edu.ar>
2021-11-09 21:59:59 -0300

    TP2 - Anti rebote

---
+++ TP2/Drivers/API/Src/API_delay.c
@@ -0,0 +1,53 @@
+/*
+ * API_delay.c
+ *
+ *  Created on: 5 nov. 2021
+ *      Author: Martin Juarez
+ */
+
+
+#include "API_delay.h"
+#include "stm32f4xx_hal.h"
+
+
+
+void delayInit( delay_t * delay, tick_t duration )
+{
+	delay->duration = duration;
+	delay->running = 0;
+}
+
+void delayStart( delay_t * delay)
+{
+	delay->running = 1;
+}
+
+
+bool_t delayRead( delay_t * delay )
+{
+	bool_t done=0;
+	uint32_t resta;
+
+	if((delay->running) == 0)
+	{
+		delay->startTime = HAL_GetTick();
+		delay->running = 1;
+	}
+	else
+	{
+		resta = HAL_GetTick() - delay->startTime;
+		if(resta >= (delay->duration))
+		{
+			done=1;
+			delay->running = 0;
+		}
+	}
+	return done;
+}
+
+
+void delayWrite( delay_t * delay, tick_t duration )
+{
+	delay->duration = duration;
+}
+
